b383d4b2426d6ebe71583960e3224f1453d9c35c
Siddharth Ajay <sid.ajay05@gmail.com>
2024-08-07 18:07:26 -0700

    added capability to get spending breakdown from mongo api

--- spend-api/index.py
+++ spend-api/index.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "hello world"
+@app.route("/spend", methods=['POST'])
+def get_spend():
+    print(request.get_json()) 
+    return '', 202
+
